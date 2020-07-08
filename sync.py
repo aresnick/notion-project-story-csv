515e2da2d31119cc47f52f6e1c9f7e27811bdefa
Alec Resnick <aresnickety@gmail.com>
2020-07-08 12:03:23 -0400

    Pull raw rows from a Notion collection

--- sync.py
+++ sync.py
@@ -1,12 +1,33 @@
 #!/usr/bin/env python3
 
+# For parsing command lines
 import argparse
+import re
 
-parser = argparse.ArgumentParser(description='Generate a CSV file from a Notion table')
+# For interacting with the Notion API, via https://github.com/jamalex/notion-py
+from notion.client import NotionClient
+
+# Parse command line arguments to extract API token and URL
+parser = argparse.ArgumentParser(
+    description='Generate a CSV file from a Notion table')
 parser.add_argument('token_v2', metavar='toven_v2', type=str,
-                   help='API token_v2, pulled from inspecting cookies in a logged-in Notion.so session')
+                    help='API token_v2, via logged-in notion.so cookies')
 parser.add_argument('url', metavar='URL', type=str,
-                   help='URL of the table to convert')
-
+                    help='URL of the table to convert')
 args = parser.parse_args()
-print(args)
+
+# Initialize client with `token_v2`
+client = NotionClient(token_v2=args.token_v2)
+
+# Access a database using the URL of the database page
+try:
+    url_regex = re.compile('https?\:\/\/www.notion.so/.+?/(.+?)(\?|\n)')
+    collection_id = url_regex.match(args.url)[1]
+    print(collection_id)
+except TypeError:
+    print(args.url, "does not appear to be a valid Notion collection URL…")
+    exit()
+
+collection_view = client.get_collection_view(args.url)
+
+print(collection_view.collection.get_rows())
